91a25934b55433eb296cbff8d26daa9caf9a74b8
jaseemery <jmemerydev@gmail.com>
2021-12-18 19:33:17 +0000

    -rails route cow

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem "sqlite3", "~> 1.4"
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
+gem 'ruby_cowsay'
+gem 'jbuilder'
 
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 # gem "jbuilder"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+ Rails.application.routes.draw do
+    post 'say', to: 'cow#say'
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
